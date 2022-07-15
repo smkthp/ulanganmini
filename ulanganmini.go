0001560e5575cb1fce4aaa33ab9688e7942449f5
Dimas Ananda <traperwaze@gmail.com>
2022-07-15 08:08:53 +0700

    prototype net listen
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- ulanganmini.go
+++ ulanganmini.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Welcome to Ulangan Mini!")
+
+	listener, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err == nil {
+			go handleConn(conn)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	conn.Write([]byte("\u001B[2J"))
+
+	conn.Write([]byte("Halo!\n"))
+
+	go func() {
+		for {
+			conn.Write([]byte(">>> "))
+			buffer := bufio.NewReader(conn)
+			msg, err := buffer.ReadString('\n')
+			if err != nil {
+				continue
+			}
+
+			if strings.HasPrefix(msg, "!exit") {
+				conn.Write([]byte("Bye!\n"))
+				conn.Close()
+			}
+		}
+	}()
 }
