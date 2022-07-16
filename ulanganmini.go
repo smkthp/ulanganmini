1219aec78a2ffce76e12cac2fae5c1dcda401012
Dimas Ananda <traperwaze@gmail.com>
2022-07-16 08:17:01 +0700

    restore. design changed
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- ulanganmini.go
+++ ulanganmini.go
@@ -1,47 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"net"
-	"strings"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Welcome to Ulangan Mini!")
-
-	listener, err := net.Listen("tcp", ":7777")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		conn, err := listener.Accept()
-		if err == nil {
-			go handleConn(conn)
-		}
-	}
-}
-
-func handleConn(conn net.Conn) {
-	conn.Write([]byte("\u001B[2J"))
-
-	conn.Write([]byte("Halo!\n"))
-
-	go func() {
-		for {
-			conn.Write([]byte(">>> "))
-			buffer := bufio.NewReader(conn)
-			msg, err := buffer.ReadString('\n')
-			if err != nil {
-				continue
-			}
-
-			if strings.HasPrefix(msg, "!exit") {
-				conn.Write([]byte("Bye!\n"))
-				conn.Close()
-			}
-		}
-	}()
 }
