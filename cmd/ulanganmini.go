2290e3a6d782bd4930fa0d15c932d40680ebdde7
Dimas Ananda <traperwaze@gmail.com>
2022-07-20 20:44:44 +0700

    fix: initialize runner with writer
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- cmd/ulanganmini.go
+++ cmd/ulanganmini.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 
 	"github.com/smkthp/ulanganmini/runner"
+	"github.com/smkthp/ulanganmini/writer"
 )
 
 func main() {
 	fmt.Println("Welcome to Ulangan Mini!")
 
 	ctx := context.Background()
-	runner := runner.NewRunner()
+	writer := writer.NewWriter()
+	runner := runner.NewRunner(writer)
 
 	runner.Run(ctx)
 }
