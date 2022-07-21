91ab84a5dbd732e029cee549bcf4a984ed1dc023
Dimas Ananda <traperwaze@gmail.com>
2022-07-21 13:36:09 +0700

    new helper unmarshal tasks
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- util/util.go
+++ util/util.go
@@ -1,9 +1,12 @@
 package util
 
 import (
+	"encoding/json"
 	"os"
 	"os/exec"
 	"runtime"
+
+	"github.com/smkthp/ulanganmini/system"
 )
 
 func ClearTerminal() {
@@ -19,4 +22,14 @@ func ClearTerminal() {
         cmd.Stdout = os.Stdout
         cmd.Run()
 	}
+}
+
+func unmarshalTasks(p []byte) ([]system.Task, error) {
+	var tasks []system.Task
+
+	if err := json.Unmarshal(p, &tasks); err != nil {
+		return tasks, err
+	}
+
+	return tasks, nil
 }
---
+++ util/util_test.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/smkthp/ulanganmini/system"
+)
+
+func TestUnmarshalTasks(t *testing.T) {
+	body := `[{"taskID":1,"title":"t1"},{"taskID":2,"title":"t2"}]`
+	want := []system.Task{
+		{ID: 1, Title: "t1"},
+		{ID: 2, Title: "t2"},
+	}
+
+	tasks, err := unmarshalTasks([]byte(body))
+	if err != nil {
+		t.Fatalf("Unmarshal error: %v", err)
+	}
+
+	if !reflect.DeepEqual(want, tasks) {
+		t.Fatalf("Got %+v, want %+v", tasks, want)
+	}
+}
